84964f7da76e8995fe07aec379630e62233adddc
benwinschel <ben@winschel.com>
2017-11-12 10:56:06 -0800

    Add files via upload

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python 
-
+#!/usr/bin/env python
 from flask import Flask, request, render_template, make_response
 from flask_restful import Resource, Api
 import rospy
@@ -8,12 +7,14 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 from multiprocessing import Process
 from threading import Thread
+from playsound import playsound
 
 
 app = Flask(__name__)
 api = Api(app)
 msg_queue = Queue.Queue()
-location = {}
+global location
+location = dict()
 
 
 def dummyCallback(msg):
@@ -44,12 +45,15 @@ class location(Resource):
 
 class locationHTML(Resource):
 	def get(self):
-		if request.args.get('lat') is not None and request.args.get('long') is not None:
-			global location
-			location = request.args
-			return make_response(render_template('location.html', lat=request.args.get('lat'), lon=request.args.get('long')))
-		else:
-			return make_response(render_template('location.html', lat=location['lat'], lon=location['long']))
+		try:
+			if request.args.get('lat') is not None and request.args.get('long') is not None:
+				global location
+				location = request.args
+				return make_response(render_template('location.html', lat=request.args.get('lat'), lon=request.args.get('long')))
+			else:
+				return make_response(render_template('location.html', lat=location['lat'], lon=location['long']))
+		except TypeError:
+				return make_response(render_template('location.html', lat=0, lon=0))
 
 class cmd(Resource):
 	def put(self):
@@ -60,7 +64,22 @@ class cmdHTML(Resource):
 	def get(self):
 		if request.args.get('cmd') == 'stop':
 			cmd_vel.publish(Twist())
-			return make_response(render_template('cmd.html'))
+			return make_response(render_template('cmd.html', speed=0))
+
+		if request.args.get('cmd') == 'go':
+			twist = Twist()
+			twist.linear.x = 0.5
+			cmd_vel.publish(twist)
+			return make_response(render_template('cmd.html', speed=twist.linear.x))
+
+class soundHTML(Resource):
+	def get(self):
+		if request.args.get('sound') == 'siren':
+			t = Thread(target=playsound, args=('/home/bwinschel/Desktop/siren.mp3',))
+			t.start()
+			return make_response(render_template('sound.html'))
+			
+
 
 api.add_resource(ats, '/ats')
 api.add_resource(location, '/location')
@@ -68,13 +87,12 @@ api.add_resource(cmd, '/cmd')
 api.add_resource(atsHTML, '/robot/ats')
 api.add_resource(locationHTML, '/robot/location')
 api.add_resource(cmdHTML, '/robot/cmd')
+api.add_resource(soundHTML, '/robot/sound')
 
 if __name__ == '__main__':
 	#app.run(debug=True)
-	#change host ip when you want to use on different device
-	t = Thread(target=app.run, kwargs={'host' : '129.64.153.118'})
+	t = Thread(target=app.run, kwargs={'host' : '192.168.204.137'})
 	t.daemon = True
-	#t.start() runs flask as its own thread
 	t.start()
 	rospy.init_node('talker')
 	rospy.spin()
